e75d4ac8f185c5969cd42eb9a964961d4e9255ca
Ironhack Education Team <admin.education@ironhack.com>
2022-04-11 17:16:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+/*
+Exercise | Hello World!
+============================
+Tasks:
+  1. Use the `console.log()` method to print `"Hello World!"` in the `console`.
+  2. Add a single line comment to your code containing your name.
+  3. Add a multi-line comment to your code with anything you want to say!
+
+*/
+
+// Write Your code Below: 
